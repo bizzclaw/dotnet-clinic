251e97d70f215e0903e8e2594c36810240a3ca93
James Osborn, Joseph Tomlinson and Mark Woodward <james+joseph+mark@>
2018-01-10 14:42:23 -0800

    add mock database templates

---
+++ Clinic/Models/Repositories/EFItemRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+	public class EFPatientRepository : IPatientRepository
+	{
+		ToDoListContext db = new ToDoListContext();
+
+		public IQueryable<Patient> patients
+		{ get { return db.patients; } }
+
+		public Patient Save(Patient patient)
+		{
+			db.Items.Add(patient);
+			db.SaveChanges();
+			return patient;
+		}
+
+		public Patient Edit(Patient patient)
+		{
+			db.Entry(patient).State = EntityState.Modified;
+			db.SaveChanges();
+			return patient;
+		}
+
+		public void Remove(Patient patient)
+		{
+			db.Items.Remove(patient);
+			db.SaveChanges();
+		}
+	}
+}
---
+++ Clinic/Models/Repositories/IPatientRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public interface IPatientRepository
+    {
+        IQueryable<Patient> Patients { get; }
+        Patient Save(Patient patient);
+        Patient Edit(Patient patient);
+        void Remove(Patient patient);
+    }
+}
